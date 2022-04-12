1bc3fb7413960c426142e0f72ae6f9c6bca9e3ce
jaflanet <jonathan.aurelius@ui.ac.id>
2022-04-12 21:08:49 +0700

    konfirmasi user update

--- src/pages/KonfirmasiUser.js
+++ src/pages/KonfirmasiUser.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography, Button, Container, Grid } from "@mui/material";
 import { DocumentService } from "../services/DocumentService";
-import { useParams } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 
 const KonfirmasiUser = () => {
   const { id } = useParams();
   const user = JSON.parse(sessionStorage.getItem("pengguna"));
   // console.log(user);
   const [document, setDocument] = useState([]);
+  const navigate = useNavigate();
   console.log(id);
   console.log(user.id_user);
 
@@ -20,7 +21,7 @@ const KonfirmasiUser = () => {
     };
 
     await DocumentService.borrowDocument(data);
-    console.log(data);
+    navigate("kelola-data/peminjaman");
 
     try {
     } catch (error) {
@@ -36,16 +37,103 @@ const KonfirmasiUser = () => {
     };
     fetchBuku();
   }, [id]);
-
+  console.log(document);
   return (
-    <>
-      <Box>
-        <Typography>{document.judul_dokumen}</Typography>
-        <Button onClick={borrow} variant="contained" color="success">
-          Add
-        </Button>
+    <Container maxWidth="xl">
+      <Typography variant="h4" fontWeight={600} gutterBottom>
+        Peminjaman
+      </Typography>
+      <Typography
+        variant="h6"
+        fontWeight={600}
+        gutterBottom
+        style={{
+          color: "#6F8197",
+        }}
+      >
+        Detail Peminjaman
+      </Typography>
+      <Box
+        sx={{
+          backgroundColor: "#FDFAFA",
+          p: 2,
+          pt: 4,
+          width: "95%",
+        }}
+      >
+        <Typography
+          variant="h6"
+          fontWeight={600}
+          style={{
+            color: "#1C375B",
+          }}
+          gutterBottom
+        >
+          Nama
+        </Typography>
+        <Typography variant="h5" fontWeight={400} gutterBottom>
+          {user.nama}
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={600}
+          style={{
+            color: "#1C375B",
+          }}
+          gutterBottom
+        >
+          Judul Dokumen
+        </Typography>
+        <Typography variant="h5" fontWeight={400} gutterBottom>
+          {document.judul_dokumen}
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={600}
+          style={{
+            color: "#1C375B",
+          }}
+          gutterBottom
+        >
+          Kategori Dokumen
+        </Typography>
+        <Typography variant="h5" fontWeight={400} gutterBottom>
+          {document.kategori_dokumen}
+        </Typography>
+        <Typography
+          variant="h6"
+          fontWeight={600}
+          style={{
+            color: "#1C375B",
+          }}
+          gutterBottom
+        >
+          PIC Dokumen
+        </Typography>
+        <Typography variant="h5" fontWeight={400} gutterBottom>
+          {document.nama_pic}
+        </Typography>
+        <Box sx={{ pt: 4, pb: 2, pr: 2 }}>
+          <Grid container spacing={4} justifyContent="flex-end">
+            <Grid item>
+              <Button
+                component={Link}
+                to={"/kelola-data/dokumen"}
+                variant="contained"
+                color="error"
+              >
+                Cancel
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button onClick={borrow} variant="contained" color="success">
+                Continue
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
-    </>
+    </Container>
   );
 };
 
